447a38839589a69cf71d36ad77ec02685bd53084
jaymalave <jaym.github@gmail.com>
2021-08-15 19:15:19 +0530

    fixed bugs

--- client/src/components/ExpenseList.js
+++ client/src/components/ExpenseList.js
@@ -1,18 +1,26 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import Expense from "./Expense";
 import {ExpenseContext} from '../context/ExpenseContext';
 
 
 const ExpenseList = () => {
+
     const [expenses, setExpenses] = useContext(ExpenseContext);
-  return (
-    <>
+
+  
+   return (
+    <div>
+    <h1>Hello</h1>
       {
-       expenses.map((expense) => {
+      
+       expenses.map((expense) => (
+           <div>
+           <h1>This is an expense</h1>
            <Expense name={expense.name} cost={expense.cost}/>
-       })
+           </div>
+       ))
       }
-    </>
+    </div>
   );
 };
 
--- client/src/context/ExpenseContext.js
+++ client/src/context/ExpenseContext.js
@@ -1,7 +1,10 @@
 import React, {useState, createContext} from "react";
 
+//context
 export const ExpenseContext = createContext();
 
+
+//provider
 export const ExpenseProvider = (props) => {
     const [expenses, setExpenses] = useState([]);
     return (
