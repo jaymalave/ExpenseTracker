78219be6bef1f1128fb49816605ae3b2159a3aa5
jaymalave <jaym.github@gmail.com>
2021-09-20 00:38:40 +0530

    modified dependencies, controllers added

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,6 +1,20 @@
 // @desc    Get all transactions
-// @route   /api/v1/transactions
+// @route   GET /api/v1/transactions
 // @access  Public
 exports.getTransactions = (req, res, next) => {
-    res.send("GET all transactions")
-}
+  res.send("GET view all transactions");
+};
+
+// @desc    Add a transaction
+// @route   POST /api/v1/transactions
+// @access  Public
+exports.addTransaction = (req, res, next) => {
+  res.send("POST add a transaction");
+};
+
+// @desc    Get all transactions
+// @route   DELETE /api/v1/transactions/:id
+// @access  Public
+exports.deleteTransaction = (req, res, next) => {
+  res.send("DELETE delete a transaction");
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const {getTransactions} = require('../controllers/transactions')
+const {getTransactions, addTransaction, deleteTransaction} = require('../controllers/transactions')
 
 router.route('/').get(getTransactions);
+router.route('/add').post(addTransaction);
+router.route('/delete/:id').delete(deleteTransaction);
+
 
 module.exports = router;
