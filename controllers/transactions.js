be6f2e29b41ec47093c09843ba3611e44013a252
jaymalave <jaym.github@gmail.com>
2021-09-20 00:22:49 +0530

    new approach towards node

---
+++ controllers/transactions.js
@@ -0,0 +1,6 @@
+// @desc    Get all transactions
+// @route   /api/v1/transactions
+// @access  Public
+exports.getTransactions = (req, res, next) => {
+    res.send("GET all transactions")
+}
--- server.js
+++ server.js
@@ -1,47 +1,48 @@
-var express = require('express')
-const ExpenseDoc = require('./models/expenses')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv/config')
-var app = express()
-const PORT_NO = 8000 || process.env.PORT
+var express = require("express");
+const ExpenseDoc = require("./models/expenses");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+var app = express();
+const PORT_NO = 8000 || process.env.PORT;
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 mongoose.connect(
-    process.env.DB_CON,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    () => {
-        console.log('database connected')
-    }
-  );
+  process.env.DB_CON,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("database connected");
+  }
+);
 
-app.get('/', async (req, res) => {
-   const allData = await ExpenseDoc.find()
-   res.json({"message": "blank route GET request working"})
-})
+const transactions = require("./routes/transactions");
 
-app.post('/addexpense', async (req, res) => {
-    const {name, cost} = req.body
-    const expense = new ExpenseDoc(
-        {
-            name,
-            cost
-        }
-    )
-    await expense.save()
-    // console.log(savedExpense)
-    console.log({name, cost})
-})
+app.use('/api/v1/transactions/', transactions);
+
+app.get("/", async (req, res) => {
+  const allData = await ExpenseDoc.find();
+  res.json({ message: "blank route GET request working" });
+});
+
+app.post("/addexpense", async (req, res) => {
+  const { name, cost } = req.body;
+  const expense = new ExpenseDoc({
+    name,
+    cost,
+  });
+  await expense.save();
+  // console.log(savedExpense)
+  console.log({ name, cost });
+});
 
 app.listen(PORT_NO, () => {
-    console.log("Server is up and running");
-})
+  console.log("Server is up and running");
+});
